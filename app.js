4635c31f0a665e7ae410af3102da1a04ec46e762
vthang <tranvietthang95@gmail.com>
2017-02-10 11:49:07 +0700

    Xoa comment thua

--- app.js
+++ app.js
@@ -9,28 +9,13 @@ const notes = require('./notes.js');
 
 var argv = yargs.argv;
 var command  = argv._[0];
-// console.log(yargs.argv);
-// console.log('Command:' + command);
+
 if (command==='play') {
 	notes.creatMap();
 	notes.randomEnemies();
 	notes.tutorial();
 } else if (command==='shot') {
 	notes.shot(argv.x, argv.y);
-	// notes.randomEnemies();
+} else {
+	console.log('Command note recognized!');
 }
-// if (command === 'add') {
-// 	notes.addNote(argv.title, argv.body);
-// } else if (command === 'list') {
-// 	notes.getAll();
-// } else if (command === 'remove') {
-// 	notes.removeNote(argv.title);
-// } else if (command === 'read') {
-// 	notes.read(argv.title);
-// } else {
-// 	console.log('command not recognized!')
-// }
-
-// var mapString = fs.readFileSync('data.json');
-// var map = JSON.parse(mapString);
-// console.log(map);
