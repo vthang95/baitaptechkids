df6246d896db40e39a6778a81e90f9f3e74fcea1
vthang <tranvietthang95@gmail.com>
2017-02-11 15:08:27 +0700

    Trinh bay lai cho ro nhin hon

--- notes.js
+++ notes.js
@@ -1,31 +1,24 @@
 const fs = require('fs');
 
 var creatMap = () => {
-	console.log('Map is created!\n')
-	try {
-		var readDt = fs.readFileSync('data.json');
-		dataObject = JSON.parse(readDt);
-	} catch (err) {
-
-	}
-	var mapDefault = fs.readFileSync('data.json');
-	var mapDefaultObject = JSON.parse(mapDefault);
-	fs.writeFileSync('datatmp.json', JSON.stringify(mapDefaultObject));
-	console.log(dataObject);
+	console.log('Map is created!\n');
+	var mapDefaultString = fs.readFileSync('./data/mapdefault.json');
+	var mapDefaultObject = JSON.parse(mapDefaultString);
+	fs.writeFileSync('./data/map.json', JSON.stringify(mapDefaultObject));
+	console.log(mapDefaultObject);
 }
 
-
 var randomEnemies = () => {
-	var dataTmp = fs.readFileSync('data.json');
-	var tmpObject = JSON.parse(dataTmp);
+	var enemyString = fs.readFileSync('./data/mapdefault.json');
+	var enemyObject = JSON.parse(enemyString);
 	while (true) {
 		var a = Math.floor((Math.random()*4+1));
 		var b = Math.floor((Math.random()*4+1));
 		var c = Math.floor((Math.random()*4+1));
 		var d = Math.floor((Math.random()*4+1));
 		if (a!=b) {
-			tmpObject[a].splice(c, 1, 'x');
-			tmpObject[b].splice(d, 1, 'x');
+			enemyObject[a].splice(c, 1, 'x');
+			enemyObject[b].splice(d, 1, 'x');
 			break;
 		}
 		if (a==b) {
@@ -33,22 +26,22 @@ var randomEnemies = () => {
 				if (c==d) {
 					c = Math.floor((Math.random()*4+1));
 				} else {
-					tmpObject[a].splice(c, 1, 'x');
-					tmpObject[b].splice(d, 1, 'x');
+					enemyObject[a].splice(c, 1, 'x');
+					enemyObject[b].splice(d, 1, 'x');
 					break;
 				}
 			}
 		}
 		break;
 	};
-	fs.writeFileSync('tmp.json', JSON.stringify(tmpObject));
 
-	// Luu so enemies con lai va so lan ban con lai
+	fs.writeFileSync('./data/enemies.json', JSON.stringify(enemyObject));
+	
 	var obj = {
 		enemy: 2,
 		rocket: 4
 	}
-	fs.writeFileSync('counting.json', JSON.stringify(obj))
+	fs.writeFileSync('./data/counting.json', JSON.stringify(obj))
 }
 
 var tutorial = () => {
@@ -58,114 +51,103 @@ var tutorial = () => {
 	console.log('Ban co 4 rocket');
 }
 
-
-
 var shot = (x, y) => {
 
 	if (x<1||y<1||y>4||x>4) {
 		console.log('Khong ton tai toa do nay. Hay nhap toa do hop le');
 		return;
 	}
-
 	console.log('Shot enemy at: ('+x,y+')');
 	
-	var counting = fs.readFileSync('counting.json');
-	var countingObject = JSON.parse(counting);
+	var countingString = fs.readFileSync('./data/counting.json');
+	var countingObject = JSON.parse(countingString);
+	var mapDataString = fs.readFileSync('./data/map.json');
+	var mapDataObject = JSON.parse(mapDataString);
+	var enemyString = fs.readFileSync('./data/enemies.json');
+	var enemyObj = JSON.parse(enemyString);
+
 	if (countingObject.rocket<=0) {
 		console.log('Hay nhap "node app play" de choi lai');
 		return;
 	}
 	countingObject.rocket--;
 
-	//Lay thong tin cua file data luu toa do enemies
-	var infoData = fs.readFileSync('datatmp.json');
-	var infoDataTmp = JSON.parse(infoData);
-	var mapDataTmp = fs.readFileSync('tmp.json');
-	var dataObj = JSON.parse(mapDataTmp);
-	// MUon cheat thi uncomment dong nay
-	// console.log(dataObj);
-
 	function enemiesAround(ngang, doc) {
 		var count = 0;
-		if (dataObj[doc][ngang-1] == 'x') {
+		if (enemyObj[doc][ngang-1] == 'x') {
 			count++;
 		}
-		if (dataObj[doc][ngang+1] == 'x') {
+		if (enemyObj[doc][ngang+1] == 'x') {
 			count++;
 		}
 		if ((doc+1)<5&&(doc-1)>=0) {
-			if (dataObj[doc-1][ngang-1] == 'x') {
+			if (enemyObj[doc-1][ngang-1] == 'x') {
 				count++;
 			}
-			if (dataObj[doc-1][ngang] == 'x') {
+			if (enemyObj[doc-1][ngang] == 'x') {
 				count++;
 			}
-			if (dataObj[doc-1][ngang+1] == 'x') {
+			if (enemyObj[doc-1][ngang+1] == 'x') {
 				count++;
 			}
-			if (dataObj[doc+1][ngang-1] == 'x') {
+			if (enemyObj[doc+1][ngang-1] == 'x') {
 				count++;
 			}
-			if (dataObj[doc+1][ngang] == 'x') {
+			if (enemyObj[doc+1][ngang] == 'x') {
 				count++;
 			}
-			if (dataObj[doc+1][ngang+1] == 'x') {
+			if (enemyObj[doc+1][ngang+1] == 'x') {
 				count++;
 			}
 		}
 		if (doc==4) {
-			if (dataObj[doc-1][ngang-1] == 'x') {
+			if (enemyObj[doc-1][ngang-1] == 'x') {
 				count++;
 			}
-			if (dataObj[doc-1][ngang] == 'x') {
+			if (enemyObj[doc-1][ngang] == 'x') {
 				count++;
 			}
-			if (dataObj[doc-1][ngang+1] == 'x') {
+			if (enemyObj[doc-1][ngang+1] == 'x') {
 				count++;
 			}
 		}	
 		return count;
 	}
 
-	if (dataObj[y][x]=='x') {
-		console.log('Trung roi! Trung roi! :O');
+	if (enemyObj[y][x]=='x') {
+		console.log('Trung roi! Trung roi! :v');
 		countingObject.enemy--;
-		infoDataTmp[y].splice(x, 1, 'o');
-		dataObj[y].splice(x, 1, '-');
-		// cap nhat lai map vao file datatmp.json
-		fs.writeFileSync('datatmp.json', JSON.stringify(infoDataTmp));
-		fs.writeFileSync('tmp.json', JSON.stringify(dataObj));
+		mapDataObject[y].splice(x, 1, 'o');
+		enemyObj[y].splice(x, 1, '-');
+		// cap nhat lai map
+		fs.writeFileSync('./data/map.json', JSON.stringify(mapDataObject));
+		fs.writeFileSync('./data/enemies.json', JSON.stringify(enemyObj));
 		console.log('so may bay xung quanh la: '+enemiesAround(x, y));
-		// console.log(dataObj);
-		// console.log(infoDataTmp);
 	} else {
-
-		infoDataTmp[y].splice(x, 1, '*');
-		// console.log(infoDataTmp);
+		mapDataObject[y].splice(x, 1, '*');
 		console.log('Ban ban truot cmnr');
-		//cap nhat lai map vao file datatmp.json
-		fs.writeFileSync('datatmp.json', JSON.stringify(infoDataTmp));
+		//cap nhat lai map
+		fs.writeFileSync('./data/map.json', JSON.stringify(mapDataObject));
 		console.log('so may bay xung quanh la: '+enemiesAround(x, y));
-		// console.log(infoDataTmp);
 	}
+
 	if (countingObject.enemy==0) {
 		console.log('Ban da thang!\nHay nhap "node app play" de choi lai');
 		return;
 	} 
-	// console.log(dataObj);	
-	console.log(infoDataTmp);
+	// in ra map hien tai!
+	console.log(mapDataObject);
 	console.log('Con lai '+countingObject.enemy+' so may bay dich!');
 	console.log('Ban con lai '+countingObject.rocket+' so rocket');
-	fs.writeFileSync('counting.json', JSON.stringify(countingObject));
+	fs.writeFileSync('./data/counting.json', JSON.stringify(countingObject));
+
 	if (countingObject.rocket==0) {
 		console.log('Ban da thua!\nMay bay dich o day co ma:');
-		console.log(dataObj);
+		console.log(enemyObj);
 		console.log('Hay nhap "node app play" de choi lai');
 		return;
 	}
 }
-
-
 
 module.exports = {
 	shot,
